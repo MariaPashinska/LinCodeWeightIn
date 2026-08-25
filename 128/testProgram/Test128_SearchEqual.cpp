935159b9eaf8b5a674c49665acfba33b6a2487cb
MariaPashinska <mariqpashinska@gmail.com>
2026-08-25 11:03:00 +0300

    Add files via upload

---
+++ 128/testProgram/Test128_SearchEqual.cpp
@@ -0,0 +1,14 @@
+﻿#include <time.h>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string.h>
+#include "testDriver.h"
+#include "LinCodeWeightInv.h"
+using namespace std;
+
+
+int main(int argc, char** argv) {
+	test_driveSSE(3);
+	return 0;
+}
